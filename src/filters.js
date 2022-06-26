bb94314047d23a8bbc7ff704899022c243717ffa
Jamie Humphries <jamie.humphries@outlook.com>
2022-06-26 13:33:23 +0100

    Commonise +1 name logic.

--- src/filters.js
+++ src/filters.js
@@ -2,11 +2,21 @@ function firstName(guest) {
   return guest.name.split(" ")[0];
 }
 
+function rsvpName(guest, allUserGuests) {
+  if (!guest.isPlusOne) {
+    return guest.name;
+  }
+
+  const mainGuest = allUserGuests.find((g) => g.isSingle);
+  return `${mainGuest.name} +1`;
+}
+
 function validationKey(guest, fieldName) {
   return `${guest.id}__${fieldName}`;
 }
 
 module.exports = {
   firstName,
+  rsvpName,
   validationKey,
 };
